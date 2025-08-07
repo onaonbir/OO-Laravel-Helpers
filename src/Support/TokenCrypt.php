f6631d9af80197687c7e00176950331e704532dd
bariskanberkay <berkaybariskan0208@gmail.com>
2025-08-08 01:54:47 +0300

    TokenCrypt Changes

--- src/Support/TokenCrypt.php
+++ src/Support/TokenCrypt.php
@@ -12,7 +12,7 @@ class TokenCrypt
      */
     public static function encrypt(string $plainText): string
     {
-        $key = substr(hash('sha256', config('app.transfer_token_secret', env('TRANSFER_TOKEN_SECRET'))), 0, 32);
+        $key = substr(hash('sha256', env('TOKENN_CRYPT_SECRET','password')), 0, 32);
         $iv = random_bytes(16);
 
         $encrypted = openssl_encrypt($plainText, 'AES-256-CBC', $key, 0, $iv);
@@ -26,7 +26,7 @@ class TokenCrypt
             $decoded = base64_decode($encoded);
             [$iv, $encrypted] = explode('::', $decoded, 2);
 
-            $key = substr(hash('sha256', config('app.transfer_token_secret', env('TRANSFER_TOKEN_SECRET'))), 0, 32);
+            $key = substr(hash('sha256', env('TOKENN_CRYPT_SECRET','password')), 0, 32);
 
             return openssl_decrypt($encrypted, 'AES-256-CBC', $key, 0, $iv);
         } catch (Throwable $e) {
