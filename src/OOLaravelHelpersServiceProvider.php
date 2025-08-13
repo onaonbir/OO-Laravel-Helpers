bd3df4dae66013fec274a4263727cf9b4e423641
bariskanberkay <berkaybariskan0208@gmail.com>
2025-08-13 16:47:42 +0300

    readable added

--- src/OOLaravelHelpersServiceProvider.php
+++ src/OOLaravelHelpersServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace OnaOnbir\OOLaravelHelpers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class OOLaravelHelpersServiceProvider extends ServiceProvider
@@ -16,5 +17,34 @@ class OOLaravelHelpersServiceProvider extends ServiceProvider
         );
     }
 
-    public function boot(): void {}
+    public function boot(): void {
+        $this->bootReadableBladeDirectives();
+    }
+
+
+    public function bootReadableBladeDirectives()
+    {
+        $prefix = 'Readable';
+
+        $this->registerBladeDirective($prefix, 'Number');
+        $this->registerBladeDirective($prefix, 'HumanNumber');
+        $this->registerBladeDirective($prefix, 'NumberToString');
+        $this->registerBladeDirective($prefix, 'Decimal');
+        $this->registerBladeDirective($prefix, 'DecInt');
+        $this->registerBladeDirective($prefix, 'Date');
+        $this->registerBladeDirective($prefix, 'DateWoYear');
+        $this->registerBladeDirective($prefix, 'Time');
+        $this->registerBladeDirective($prefix, 'DateTime');
+        $this->registerBladeDirective($prefix, 'DiffDateTime');
+        $this->registerBladeDirective($prefix, 'TimeLength');
+        $this->registerBladeDirective($prefix, 'DateTimeLength');
+        $this->registerBladeDirective($prefix, 'Size');
+    }
+
+    protected function registerBladeDirective(string $prefix, string $method): void
+    {
+        Blade::directive($prefix.$method, function ($data) use ($method) {
+            return "<?php echo OnaOnbir\\OOReadable\\Readable::get{$method}($data); ?>";
+        });
+    }
 }
